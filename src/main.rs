8c8cd3e5453f4c76d92416e39ac928633aa5399c
Vitali Tatarintev <Kalastiuz@gmail.com>
2025-10-12 11:45:20 +0200

    Add --once option

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,18 @@ use clap::Parser;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
-struct Args {}
+struct Args {
+    #[arg(long, help = "Run once and print port table, then exit")]
+    once: bool,
+}
 
 fn main() {
-    Args::parse();
+    let cli = Args::parse();
+    if cli.once {
+        println!("Running --once");
+    } else {
+        println!("Running default");
+    }
+
     println!("Portwatch");
 }
