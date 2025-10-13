6c3b5fcb2f22b04759a1c2c6ddb1bd92ecdee17c
Vitali Tatarintev <Kalastiuz@gmail.com>
2025-10-13 18:38:04 +0200

    feat: add interval flag
    Add an --interval flag to run the program with a refresh interval in
    seconds. Cannot use together with --once.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,30 @@
 use clap::Parser;
 
 #[derive(Parser, Debug)]
-#[command(version, about, long_about = None)]
+#[command(version, about = "Monitor which processes are bound to which ports", long_about = None)]
 struct Args {
-    #[arg(long, help = "Run once and print port table, then exit")]
+    #[arg(
+        long,
+        help = "Run once and print port table, then exit",
+        conflicts_with = "interval"
+    )]
     once: bool,
+
+    #[arg(
+        long,
+        help = "Run and refresh with a specified interval in seconds",
+        value_parser= clap::value_parser!(u32).range(1..60*60*24),
+        conflicts_with="once"
+    )]
+    interval: Option<u32>,
 }
 
 fn main() {
     let cli = Args::parse();
     if cli.once {
         println!("Running --once");
+    } else if let Some(interval) = cli.interval {
+        println!("Running --interval with {} seconds", interval);
     } else {
         println!("Running default");
     }
