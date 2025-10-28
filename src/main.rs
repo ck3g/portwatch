ce5ceaab34283bd88ba9dcb9214c7d065656c946
Vitali Tatarintev <Kalastiuz@gmail.com>
2025-10-28 22:34:43 +0100

    Print fake list of ports information

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,21 @@ struct Args {
     interval: Option<u32>,
 }
 
+#[derive(Debug, Clone)]
+enum Proto {
+    Tcp,
+    Udp,
+}
+
+#[derive(Debug, Clone)]
+struct PortProc {
+    proto: Proto,
+    local_address: String,
+    pid: u32,
+    proc_name: String,
+    state: Option<String>,
+}
+
 fn main() {
     let cli = Args::parse();
     if cli.once {
@@ -34,7 +49,12 @@ fn main() {
 
 fn handle_once() {
     match fetch_ss_output() {
-        Ok(stdout) => println!("{stdout}"),
+        Ok(stdout) => {
+            let pp = scan_ports(stdout);
+            for p in pp {
+                println!("{:?}", p);
+            }
+        }
         Err(err) => eprintln!("scan failed: {err}"),
     }
 }
@@ -53,4 +73,26 @@ fn fetch_ss_output() -> anyhow::Result<String> {
     );
 
     Ok(String::from_utf8_lossy(&output.stdout).into_owned())
+}
+
+fn scan_ports(output: String) -> Vec<PortProc> {
+    println!("{}", output);
+    let pp = vec![
+        PortProc {
+            proto: Proto::Udp,
+            local_address: String::from("224.0.0.251:5353"),
+            pid: 123,
+            proc_name: String::from("chromium"),
+            state: Some("UNCONN".to_string()),
+        },
+        PortProc {
+            proto: Proto::Tcp,
+            local_address: String::from("127.0.0.1:6000"),
+            pid: 123,
+            proc_name: String::from("FakeProc"),
+            state: Some("LISTEN".to_string()),
+        },
+    ];
+
+    pp
 }
